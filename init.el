c3297767cb48c64328c00bb6e6b1dc56a9e78587
0247983545 <0247983545@ci121148m.ecc.u-tokyo.ac.jp>
2013-01-18 09:42:34 +0900

    added yatex

---
+++ init.el
@@ -0,0 +1,31 @@
+;;背景色の設定
+(custom-set-faces
+'(default ((t
+(:background "#000040" :foreground "#e0e0e0")
+)))
+'(cursor ((((class color)
+(background dark))
+(:background "#00AA00"))
+(((class color)
+(background light))
+(:background "#999999"))
+(t ())
+)))
+
+;; C-x cでM-x compileになる
+(global-set-key "\C-xc" 'compile)
+ 
+;; C-h でbackspaceにする
+(global-set-key "\C-h" 'delete-backward-char)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Setting for YaTeX
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq load-path (cons (expand-file-name "~/.emacs.d/yatex") load-path))
+
+(setq auto-mode-alist
+      (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
+(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
