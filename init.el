ebe5b1226026d6a85baa3002257d8770f4e6fb17
Masaki Waga <tsugarutamenobu@gmail.com>
2013-02-28 07:19:13 +0900

    haskell installed

--- init.el
+++ init.el
@@ -29,3 +29,17 @@
 (setq auto-mode-alist
       (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
 (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Setting for haskell-mode
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;[
+(setq load-path (cons (expand-file-name "~/.emacs.d/haskell-mode/") load-path))
+(require 'haskell-mode)
+(require 'haskell-cabal)
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'font-lock-mode)
+(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
