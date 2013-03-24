d7e703d1781cb2ee4c5033ebf2c2d09119f81740
Masaki Waga <tsugarutamenobu@gmail.com>
2013-03-25 01:18:35 +0900

    initの分離を行った。

--- init.el
+++ init.el
@@ -18,28 +18,9 @@
 ;; C-h でbackspaceにする
 (global-set-key "\C-h" 'delete-backward-char)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;   Setting for YaTeX
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq load-path (cons (expand-file-name "~/.emacs.d/yatex") load-path))
+;; yatexの設定ファイルを読み込む
+(load-file "~/.emacs.d/init4yatex.el")
 
-(setq auto-mode-alist
-      (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
-(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;   Setting for haskell-mode
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;[
-(setq load-path (cons (expand-file-name "~/.emacs.d/haskell-mode/") load-path))
-(require 'haskell-mode)
-(require 'haskell-cabal)
-(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(add-hook 'haskell-mode-hook 'font-lock-mode)
-(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
+;; haskell-modeの設定ファイルを読み込む
+(load-file "~/.emacs.d/init4haskell-mode.el")
---
+++ init4yatex.el
@@ -0,0 +1,11 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Setting for YaTeX
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq load-path (cons (expand-file-name "~/.emacs.d/yatex") load-path))
+
+(setq auto-mode-alist
+      (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
+(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
