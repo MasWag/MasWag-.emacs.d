905c51a6edcd3de7e9666b398855d36da11280e6
Masaki Waga <tsugarutamenobu@gmail.com>
2013-11-04 14:08:54 +0900

    あああああ

---
+++ init4mediawiki-mode.el
@@ -0,0 +1,5 @@
+(require 'mediawiki)
+(setq mediawiki-site-alist
+      (append '("RoboTechWiki" "https://robotich.ath.cx/wiki/" "Maswag" "psycholoid" "Main Page")
+              mediawiki-site-alist))
+(setq mediawiki-site-default "RoboTechWiki")
