3ac62f8c499b7416e41081aa10b6d4d61d5ebb60
Masaki Waga <masakiwaga+robotech@gmail.com>
2014-01-31 13:10:40 +0900

    modified not to change bgcolor when cui-mode

--- setting4appearance.el
+++ setting4appearance.el
@@ -1,15 +1,16 @@
 ;;背景色の設定
-(custom-set-faces
-'(default ((t
-(:background "#000040" :foreground "#e0e0e0")
-)))
-'(cursor ((((class color)
-(background dark))
-(:background "#00AA00"))
-(((class color)
-(background light))
-(:background "#999999"))
-(t ())
-)))
+( when window-system 
+  (custom-set-faces
+   '(default ((t
+	       (:background "#000040" :foreground "#e0e0e0")
+	       )))
+   '(cursor ((((class color)
+	       (background dark))
+	      (:background "#00AA00"))
+	     (((class color)
+	       (background light))
+	      (:background "#999999"))
+	     (t ())
+	     ))))
 ;; tool-barを消す
 (tool-bar-mode 0)
