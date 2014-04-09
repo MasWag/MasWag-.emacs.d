c1e484e465c4772dc4dfbd5fcfd3467d2262921b
Masaki Waga <masakiwaga@gmail.com>
2014-04-09 10:53:28 +0900

    added flymake for ocaml

--- inits/30-flymake.el
+++ inits/30-flymake.el
@@ -41,3 +41,27 @@ temp-file
 (add-hook 'c-mode-hook
 '(lambda ()
 (flymake-mode t)))
+
+
+;; setting for ocaml
+(defun flymake-ocaml-init ()
+  (flymake-simple-make-init-impl
+   'flymake-create-temp-with-folder-structure nil nil
+   (file-name-nondirectory buffer-file-name)
+   'flymake-get-ocaml-cmdline))
+(defun flymake-get-ocaml-cmdline (source base-dir)
+  (list "ocaml_flycheck.pl"
+	(list source base-dir)))
+
+(push '(".+\\.ml[yilp]?$" flymake-ocaml-init flymake-simple-java-cleanup)
+      flymake-allowed-file-name-masks)
+(push
+ '("^\\(\.+\.ml[yilp]?\\|\.lhs\\):\\([0-9]+\\):\\([0-9]+\\):\\(.+\\)"
+   1 2 3 4) flymake-err-line-patterns)
+
+;; optional setting
+;; if you want to use flymake always, then add the following hook.
+;; (add-hook
+;;  'tuareg-mode-hook
+;;  '(lambda ()
+;;     (if (not (null buffer-file-name)) (flymake-mode))))
