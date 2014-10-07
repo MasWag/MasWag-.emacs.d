e2bffa62d6dfc133ad8c674ff47b00db0adc251e
Masaki Waga <masakiwaga@gmail.com>
2014-10-07 10:50:41 +0900

    added setting for helm

---
+++ inits/40-helm.el
@@ -0,0 +1,17 @@
+(require 'helm-files)
+(require 'helm-config)
+(require 'helm-ag)
+(require 'helm-descbinds)
+
+(helm-descbinds-mode)
+
+(global-set-key (kbd "C-;") 'helm-mini)
+(global-set-key (kbd "C-c h") 'helm-mini)
+(global-set-key (kbd "C-c b") 'helm-descbinds)
+(global-set-key (kbd "C-c o") 'helm-occur)
+(global-set-key (kbd "C-c s") 'helm-ag)
+(global-set-key (kbd "C-c y") 'helm-show-kill-ring)
+
+(global-set-key (kbd "M-g .") 'helm-ag)
+(global-set-key (kbd "M-g ,") 'helm-ag-pop-stack)
+(global-set-key (kbd "C-M-s") 'helm-ag-this-file)
