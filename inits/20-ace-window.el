a218426354c218b7981705c89fb631f39ebd1f43
Masaki Waga <masakiwaga@gmail.com>
2019-07-08 10:02:08 +0900

    ace window

---
+++ inits/20-ace-window.el
@@ -0,0 +1,6 @@
+(use-package ace-window
+  :custom
+  (aw-keys '(?j ?k ?l ?i ?o ?h ?y ?u ?p))
+  :bind (("C-c w" . ace-window))
+  :custom-face
+  (aw-leading-char-face ((t (:height 4.0 :foreground "#f1fa8c")))))
