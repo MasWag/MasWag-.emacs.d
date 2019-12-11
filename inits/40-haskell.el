cd4102fbb7cda427c843b7d8bc20ed871d2fecdb
Masaki Waga <masakiwaga@gmail.com>
2019-12-11 19:04:29 +0900

    fixed the key bindings

--- inits/40-haskell.el
+++ inits/40-haskell.el
@@ -27,28 +27,27 @@
     (add-hook 'haskell-mode-hook 'font-lock-mode)
     (add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
     )
-
+  (add-hook 'haskell-mode-hook
+            (lambda ()
+              (define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
+              (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)
+              (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile)
+              (define-key haskell-mode-map (kbd "C-x C-d") 'nil)
+              (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+              (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+              (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+              (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+              (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+              (define-key haskell-mode-map (kbd "C-c M-.") 'nil)
+              (define-key haskell-mode-map (kbd "C-c C-d") 'nil)))
   :hook
   (haskell-mode . lsp-mode)
   :config
   (setq lsp-haskell-process-path-hie "~/.local/bin/hie-wrapper")
-
-  :bind (("C-," . haskell-move-nested-left)
-         ("C-." . haskell-move-nested-right)
-         ("C-c C-c" . haskell-compile)
-         ("C-x C-d" . nil)
-         ("C-c C-z" . haskell-interactive-switch)
-         ("C-c C-l" . haskell-process-load-file)
-         ("C-c C-b" . haskell-interactive-switch)
-         ("C-c C-t" . haskell-process-do-type)
-         ("C-c C-i" . haskell-process-do-info)
-         ("C-c M-." . nil)
-         ("C-c C-d" . nil))
-
+  :bind
   :mode (("\\.hs$" . haskell-mode)
          ("\\.lhs$" . literate-haskell-mode)
-         ("\\.cabal\\'" . haskell-cabal-mode))
-)
+         ("\\.cabal\\'" . haskell-cabal-mode)))
 
 (provide '40-haskell)
 ;;; 40-haskell.el ends here
