7a00a54236391857eeb547847c3b9c0381778fae
Masaki Waga <masakiwaga@gmail.com>
2019-12-11 19:10:54 +0900

    rewrote magit configuration with use-package

--- inits/30-magit.el
+++ inits/30-magit.el
@@ -1,6 +1,17 @@
-(require 'magit)
-(require 'magit-gitflow)
-(global-set-key "\C-xgs" 'magit-status)
-(global-set-key "\C-xgb" 'magit-show-refs-popup)
+;;; 30-magit --- Setting for magit
+;;; Commentary:
 
-(add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
+;;; Code:
+(require 'use-package)
+(use-package magit
+  :init
+  (require 'magit-gitflow)
+  (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
+  :bind (("\C-xgs" . magit-status)
+          ("\C-xgb" . magit-show-refs-popup))
+  :hook
+  (magit . magit-gitflow))
+
+;;; 30-magit ends here
+
+
