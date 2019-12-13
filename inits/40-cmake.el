225057fc003724f2b8d66ac33c7b0796b0f2fdaa
Masaki Waga <masakiwaga@gmail.com>
2019-12-13 11:04:52 +0900

    fixed the configuration on cmake

--- inits/40-cmake.el
+++ inits/40-cmake.el
@@ -1,6 +1,12 @@
 ;;; Code:
 (use-package company-cmake
+  :commands (company-cmake)
   :init
-  (setq company-cmake-executable
-        (s-chomp (shell-command-to-string "command -v cmake"))))
-
+  (add-hook 'cmake-mode-hook
+            (lambda ()
+              (require 's)
+              ;; Delay the configuration of the cmake executable to when cmake-mode is used
+              ;; Otherwise, shell-command-to-string does not work somehow
+              (setq company-cmake-executable
+                    (s-chomp
+                     (shell-command-to-string "command -v cmake"))))))
