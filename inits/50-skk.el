5a14aaffb737b900f01aaebc37fbc94966664c3d
Masaki Waga <masakiwaga@gmail.com>
2020-03-18 19:31:46 +0900

    updated skk config

--- inits/50-skk.el
+++ inits/50-skk.el
@@ -1,3 +1,5 @@
 ;; skk
 (setq default-input-method "japanese-skk")
 (define-key global-map [hiragana-katakana] 'skk-toggle-kana)
+(setq skk-preload t)
+(setq skk-large-jisyo "~/.emacs.d/skk-get-jisyo/SKK-JISYO.L")
