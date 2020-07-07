cdf6c6c24c6f54aee0ec73338bb400f22df353bb
Masaki Waga <masakiwaga@gmail.com>
2020-07-07 16:03:46 +0900

    show numbers in company

--- inits/20-company.el
+++ inits/20-company.el
@@ -9,6 +9,7 @@
 (setq company-idle-delay 0) ; デフォルトは0.5
 (setq company-minimum-prefix-length 2) ; デフォルトは4
 (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+(setq company-show-numbers t)
 
 ;; (add-to-list 'company-backends 'company-ghc)
 
