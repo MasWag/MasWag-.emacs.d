e30b8ad7286233a834709af1ca180af5de544570
Masaki Waga <masakiwaga@gmail.com>
2020-09-15 09:00:52 +0900

    added elpy

--- inits/40-python.el
+++ inits/40-python.el
@@ -1,2 +1,14 @@
 (setq python-shell-completion-native t)
 (add-to-list 'python-shell-completion-native-disabled-interpreters "python")
+
+;;; elpy (melpa-stable)
+(use-package elpy
+  :ensure t
+  :defer t
+  :init
+  (advice-add 'python-mode :before 'elpy-enable)
+  :config
+  (setq elpy-rpc-virtualenv-path 'current)
+  (when (load "flycheck" t t)
+    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+    (add-hook 'elpy-mode-hook 'flycheck-mode)))
