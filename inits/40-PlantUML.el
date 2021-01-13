756c8df7630ff0dfa83baa458fc07c34b73bc9a8
Masaki Waga <masakiwaga@gmail.com>
2021-01-13 17:04:19 +0900

    added a configuration of PlantUML

---
+++ inits/40-PlantUML.el
@@ -0,0 +1,19 @@
+;;; 40-PlantUML ---  Setting for PlantUML
+;;; Commentary:
+
+;;; Code:
+(use-package plantuml-mode
+  
+  :commands plantuml-mode
+  :mode (("\\.pu$" . plantuml-mode)
+         ("\\.plantuml$" . plantuml-mode)
+         ("\\.puml$" . plantuml-mode))
+  :config
+  (setq plantuml-default-exec-mode 'executable)
+  (cond ((equal (system-name) "MacBook-Pro-3.local")
+         (setq plantuml-executable-path "/usr/local/bin/plantuml")
+         (setq plantuml-jar-path "/usr/local/Cellar/plantuml/1.2021.0/libexec/plantuml.jar")))
+  (setq plantuml-jar-args "-charset UTF-8")
+  )
+(provide '40-PlantUML)
+;;; 40-PlantUML.el ends here
