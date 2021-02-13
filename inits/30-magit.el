d8ab9e460e6b930e53f10b9da73ad588013f7a67
Masaki Waga <masakiwaga@gmail.com>
2021-02-13 11:42:33 +0900

    add dependencies on magithub and magit-gh-pulls

--- inits/30-magit.el
+++ inits/30-magit.el
@@ -8,11 +8,17 @@
   :init
   (use-package magit-gitflow
     :ensure t)
+  (use-package magithub
+    :ensure t)
+  (use-package magit-gh-pulls
+    :ensure t)
   (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
   :bind (("\C-xgs" . magit-status)
           ("\C-xgb" . magit-show-refs-popup))
   :hook
-  (magit . magit-gitflow))
+  (magit . magit-gitflow)
+  (magit . magit-gh-pulls)
+  (magit . magithub))
 
 ;;; 30-magit ends here
 
