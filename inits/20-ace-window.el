9f10f2899b91fee3bf01437577e3b9802790e663
Masaki Waga <masakiwaga@gmail.com>
2021-05-12 11:47:23 +0900

    added a configuration on ace-window

--- inits/20-ace-window.el
+++ inits/20-ace-window.el
@@ -1,6 +1,11 @@
+;;; 20-ace-window.el --- Configuration for ace-window
+;;; Commentary:
+;;; Code:
 (use-package ace-window
   :custom
   (aw-keys '(?j ?k ?l ?i ?o ?h ?y ?u ?p))
   :bind (("C-c w" . ace-window))
   :custom-face
   (aw-leading-char-face ((t (:height 4.0 :foreground "#f1fa8c")))))
+(provide '20-ace-window)
+;;; 20-ace-window.el ends here
