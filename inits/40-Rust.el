a5fcdbb7bddec3d6db5bd8543086f5b71a1f2614
Masaki Waga <masakiwaga@gmail.com>
2021-10-19 09:27:00 +0900

    updated the configuration on Rust

--- inits/40-Rust.el
+++ inits/40-Rust.el
@@ -4,16 +4,27 @@
 ;;; Code:
 
 (use-package rust-mode
+  :ensure t
   :init (add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
   (add-hook 'rust-mode-hook (lambda ()
                               (racer-mode)
                               (flycheck-rust-setup)))
   :mode (("\\.rs\\'" . rust-mode))
-  :config
-  (setq-default rust-format-on-save t)
-  (cond ((equal (system-name) "Masakis-MacBook-Pro.local")
-         (setq racer-rust-src-path "~/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src")))
-  )
+  :custom rust-format-on-save t)
+
+(use-package cargo
+  :ensure t
+  :hook (rust-mode . cargo-minor-mode))
+
+(use-package lsp-mode
+  :ensure t
+  :init (yas-global-mode)
+  :hook (rust-mode . lsp)
+  :bind ("C-c h" . lsp-describe-thing-at-point)
+  :custom (lsp-rust-server 'rust-analyzer))
+
+(use-package lsp-ui
+  :ensure t)
 
 (use-package racer
   :commands racer-mode
@@ -21,6 +32,9 @@
   ;;; racerのeldocサポートを使う
   (add-hook 'racer-mode-hook #'eldoc-mode)
   (add-hook 'racer-mode-hook #'company-mode)
+  :config
+  (cond ((equal (system-name) "Masakis-MacBook-Pro.local")
+         (setq racer-rust-src-path "~/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src")))
   )
 
 (provide '40-Rust)
